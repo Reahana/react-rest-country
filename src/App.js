95951afa6da89c097e34669742fd16fbafa4bcd0
Reahana <reahana233@gmail.com>
2023-07-11 13:25:58 +0600

    app js

--- src/App.js
+++ src/App.js
@@ -5,25 +5,37 @@ import { useEffect, useState } from 'react';
 function App() {
   return (
     <div className="App">
-    <LoadCountries></LoadCountries>
+   
     </div>
   );
 }
+{/* <LoadCountries></LoadCountries> */}
+// function LoadCountries(){
+//   const [countries,setCountries]= useState([])
 
-function LoadCountries(){
-  const [countries,setCountries]= useState([])
-
-  useEffect(()=>{
-    fetch('https://restcountries.com/v3.1/all')
-    .then(res=>res.json())
-    .then(data=>setCountries(data))
-  },[])
-  return(
-    <div>
-      <h1>Visiting Every Countries</h1>
-      <h3>Available Country : {countries.length}</h3>
-    </div>
-  );
-}
+//   useEffect(()=>{
+//     fetch('https://restcountries.com/v3.1/all')
+//     .then(res=>res.json())
+//     .then(data=>setCountries(data))
+//   },[])
+//   return(
+//     <div>
+//       <h1>Visiting Every Countries</h1>
+//       <h3>Available Country : {countries.length}</h3>
+//        {
+//         countries.map(country => <Country name={country.name.common} population={country.population} capital= {country.capital}></Country>)
+//       }
+//     </div>
+//   );
+// }
+// function Country(props){
+//   return (
+//     <div>
+//       <h2>Name: {props.name}</h2>
+//       <h4>Population: {props.population}</h4>
+//       <h3>Capital: {props.capital}</h3>
+//     </div>
+//   )
+// }
 
 export default App;
