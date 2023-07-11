b40abe98dcedceab84390f4a3a95e5f3827167b4
Reahana <reahana233@gmail.com>
2023-07-11 17:30:24 +0600

    country

--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Country.css'
 
 const Country = (props) => {
+    const {area, region, name, flags , capital} = props.country;
+
     return (
         <div className='country'>
-            <h2>Country Name : {props.name}</h2>
-            <p>Population: {props.population}</p>
+            <h2>Name: {name.common}</h2>
+            <img src={flags.png} alt="" />
+            <p>Capital: {capital}</p>
+            <p>Area: {area}</p>
+            <p><small>Region: {region}</small></p>
         </div>
     );
 };
